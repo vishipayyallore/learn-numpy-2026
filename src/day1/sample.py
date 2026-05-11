7f3fa716ba612f78285610e02532972e5c69a8a1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2026-05-11 23:07:26 +0530

    Refactor Day 1 sample to demonstrate range basics and element-wise multiplication using NumPy

--- src/day1/sample.py
+++ src/day1/sample.py
@@ -1,31 +1,29 @@
-The Power of NumPy
-range(0, 5)
-range(0, 5)
-list(range(0, 5))
-[0, 1, 2, 3, 4]
-list(range(0, 5, 2))
-[0, 2, 4]
-list1 = [1, 3, 5, 7, 9]
-list2 = [2, 4, 6, 8, 10]
-list1 * list2
----------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-<ipython-input-7-7c0157aad319> in <module>
-----> 1 list1 * list2
+"""Day 1 sample: range basics and NumPy vectorized multiplication."""
 
-TypeError: can't multiply sequence by non-int of type 'list'
-new_list = []
-
-for i in range(0, len(list1)):
-    x = list1[i] * list2[i]
-    new_list.append(x)
-
-print(new_list)
-[2, 12, 30, 56, 90]
 import numpy as np
-x = np.array([1, 3, 5, 7, 9])
-y = np.array([2, 4, 6, 8, 10])
-print(type(x))
-<class 'numpy.ndarray'>
-x * y
-array([ 2, 12, 30, 56, 90])
+
+
+def main() -> None:
+    print("The Power of NumPy")
+
+    print("range(0, 5) ->", list(range(0, 5)))
+    print("range(0, 5, 2) ->", list(range(0, 5, 2)))
+
+    list1 = [1, 3, 5, 7, 9]
+    list2 = [2, 4, 6, 8, 10]
+
+    # Python lists need an explicit loop for element-wise multiplication.
+    new_list: list[int] = []
+    for i in range(len(list1)):
+        new_list.append(list1[i] * list2[i])
+    print("Element-wise with loop ->", new_list)
+
+    x = np.array([1, 3, 5, 7, 9])
+    y = np.array([2, 4, 6, 8, 10])
+
+    print("type(x) ->", type(x))
+    print("NumPy vectorized multiply ->", x * y)
+
+
+if __name__ == "__main__":
+    main()
